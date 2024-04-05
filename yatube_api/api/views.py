b96b1fc93565ad98c103d47bf05a2079cfc299a5
Rumiantsev Seveli <138662066+qqyall@users.noreply.github.com>
2024-04-05 20:28:12 +0500

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status, viewsets
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from posts.models import Comment, Group, Post, User
 
